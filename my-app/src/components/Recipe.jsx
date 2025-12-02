a869b638b301a9de04b61b88c6b0aee49c82f3fd
Aditya Singh <singhaditya082006@gmail.com>
2025-12-02 11:06:37 +0000

    recipe

---
+++ my-app/src/components/Recipe.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react"
+
+export default function Recipe()
+{
+
+    const[recipename,setRecipeName]=useState("");
+    const[ingrediants,setIngrediants]=useState("");
+    const[instructions,setInstructions]=useState("");
+    const[recipes,setRecipes]=useState([]);
+
+    //save rcecipe to localstorage
+    const saveRecipes=(updatedRecipes)=>{
+        localStorage.setItem("recipes",JSON.stringify(updatedRecipes));
+        setRecipes(updatedRecipes);
+    }
+
+    //Load recipes
+
+    //Add recipes
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        if(!recipename ||!ingrediants ||!instructions)
+        {
+            alert("please fill all fields");
+            return;
+        }
+        else
+        {
+            const newRecipe={
+                id:Date.now(),
+                name:recipename,
+                ingrediants,
+                instructions
+            }
+            saveRecipes([...recipes,newRecipe]);
+        }
+        setRecipeName("");
+        setIngrediants("");
+        setInstructions("");
+    }
+
+    return(
+        <>
+        <div style={{maxWidth:"600px",margin:"20px auto",border:"1px solid #ccc",padding:"30px"}}>
+        <h1 style={{mardinbottom:"20px",border:"1px solid #ccc",padding:"20px"}}>Recipe Book</h1>
+        <form>
+            <div style={{marginBottom:"10px"}}>
+                <label>Name:</label><br />
+                <input type="text" style={{width:"100%",padding:"5px"}}/>
+            </div>
+
+            <div style={{marginBottom:"10px"}}>
+                <label>Ingrediants:</label><br />
+                <input type="text" style={{width:"100%",padding:"5px"}}/>
+            </div>
+
+            <div style={{marginBottom:"10px"}}>
+                <label>Instructions:</label><br />
+                <input type="text" style={{width:"100%",padding:"5px"}}/>
+            </div>
+
+            <button style={{padding:"5px 10px"}} type="submit">Add Recipe</button>
+        </form>
+        </div>
+        </>
+    )
+}
